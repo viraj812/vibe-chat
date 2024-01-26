b6da9c6377104919dc12ac603ed80eb036282219
viraj812 <toviraj08@gmail.com>
2024-01-26 19:36:14 +0530

    socket update

--- src/socket.js
+++ src/socket.js
@@ -3,6 +3,6 @@ import * as io from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
 // const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:4001/';
 
-const socket = io('http://localhost:4001/', { transports: ['polling'] });
+const socket = io('https://vibe-chat-server-2jl6.onrender.com/', { transports: ['polling'] });
 
 export default socket;
