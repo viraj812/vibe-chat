caee3b39e5de9b560c0b7eb379f19e5219d1db30
viraj812 <toviraj08@gmail.com>
2024-05-15 18:13:05 +0530

    link update

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import * as io from 'socket.io-client';
 
-const socket = io('https://ec2-13-235-95-222.ap-south-1.compute.amazonaws.com:4001', { transports: ['polling'] });
+const socket = io('http://ec2-13-235-95-222.ap-south-1.compute.amazonaws.com/', { transports: ['polling'] });
 
 export default socket;
