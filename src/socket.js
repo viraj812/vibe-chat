5873d0779177d366078275f97ee40af4d8c65b91
viraj812 <toviraj08@gmail.com>
2024-11-04 13:27:00 +0530

    port

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import * as io from 'socket.io-client';
  
-const socket = io('vibe-chat-server-2jl6.onrender.com:8080', { transports: ['polling'] });
+const socket = io('vibe-chat-server-2jl6.onrender.com', { transports: ['polling'] });
 
 // const socket = io('localhost:8080', { transports: ['websocket', 'polling'] });
 
