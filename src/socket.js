0bfb28200fa74b95720b7cd97badf00d5748815d
viraj812 <toviraj08@gmail.com>
2024-11-04 12:05:39 +0530

    fix

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import * as io from 'socket.io-client';
  
-const socket = io('https://vibe-chat-server-2jl6.onrender.com', { transports: ['websocket', 'polling'] });
+const socket = io('https://vibe-chat-server-2jl6.onrender.com/', { transports: ['websocket', 'polling'] });
 
 export default socket;
