f568d3e03f10ddd0d56f782ba149b5cd39d606f5
Viraj <124186730+viraj812@users.noreply.github.com>
2025-03-11 00:54:56 +0530

    Update server.js

--- server_files/server.js
+++ server_files/server.js
@@ -6,7 +6,8 @@ const httpServer = createServer();
 
 const io = new Server(httpServer, {
     cors: {
-        origin: ["https://localhost:3000", "https://vibe-chat.onrender.com", "http://localhost:3000", "https://vibe-chat.onrender.com"],
+        origin: ["https://localhost:3000", "https://vibe-chat.onrender.com", "http://localhost:3000", "http://vibe-chat.onrender.com"],
+        method: ['GET', 'POST'],
         credentials: true
     },
     allowEIO3: true
